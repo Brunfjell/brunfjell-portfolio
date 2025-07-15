00b101ef898e2486188a2174dcf22d07a26e2d02
Brunfjell <luistroyparaiso@gmail.com>
2025-07-15 11:53:00 +0800

    v1.3.1 smol update

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,9 +1,42 @@
+import { FaReact } from 'react-icons/fa';
+import { SiVite, SiGithub, SiGit, SiNodedotjs } from 'react-icons/si';
+
 const Footer = ({ year }) => {
   return (
     <footer className="bg-stone-900 text-white py-8">
-      <div className="container mx-auto px-6 text-center">
-        <p>©{year} Brunfjell. All rights reserved.</p>
-      </div>
+      <div className=" pt-8 text-sm text-stone-700">
+        <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center text-center md:text-left gap-6">
+            
+            <div>
+              <p className="font-semibold">&copy; {year} BRUNFJELL</p>
+              <p>All rights reserved.</p>
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <p className="font-bold">Made by</p>
+                <a 
+                  href="https://brunfjell.github.io/brunfjell-portfolio/" 
+                  className="font-bold font-sans text-lg text-center" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
+                  BRUNFJELL
+                </a>
+              </div>
+              <div>
+                <p className="font-bold">Made with</p>
+                <div className="flex items-center gap-2">
+                  <FaReact className="h-6 w-6 text-stone-700" />
+                  <SiVite className="h-6 w-6 text-stone-700" />
+                  <SiNodedotjs className="h-6 w-6 text-stone-700" />
+                  <SiGithub className="h-6 w-6 text-stone-700" />
+                  <SiGit className="h-6 w-6 text-stone-700" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     </footer>
   )
 }
--- src/data/projects.js
+++ src/data/projects.js
@@ -4,8 +4,17 @@ export const projects = [
     title: 'Larong Bata',
     description: 'A game where I attempt to recreate the street games we used to play as a kid.​',
     tags: ['Godot', 'Casual', 'Filipino'],
-    image: 'assets/images/larong-bata-teaser.png',
+    image: 'https://img.itch.zone/aW1nLzIyMDcxODk1LnBuZw==/315x250%23c/zsiQ0P.png',
     liveUrl: 'https://brunfjell.itch.io/larong-bata',
     codeUrl: null
+  },
+  {
+    id: 2,
+    title: 'WanderGo',
+    description: 'WanderGo Car Portal is a modern, mobile-friendly web application for car rental services, built using React, Vite, Firebase (Firestore, Authentication, Hosting), and Tailwind CSS.​',
+    tags: ['Car Rental', 'React + Vite', 'Firebase'],
+    image: './assets/images/WanderGo.png',
+    liveUrl: 'https://wandergo-carrental.firebaseapp.com/',
+    codeUrl: 'https://github.com/Brunfjell/wandergo'
   }
 ]
